5d5efb3797e448bde1cb572d0ecda8529209c872
patrick simmons <hadroncollides@gmail.com>
2021-01-26 18:26:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,6 @@ end
 def find_valid_calls(planeteer_calls)
  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 planeteer_calls.find do |valid|
-    cheeses_type.include?(maybe_cheese)
+    valid_calls.include?(valid)
   end
 end
