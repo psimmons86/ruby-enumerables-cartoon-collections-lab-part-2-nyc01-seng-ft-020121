6ec2252fd124dd2292af92314074f4446041b039
patrick simmons <hadroncollides@gmail.com>
2021-01-26 17:46:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,19 +2,16 @@ def square_array(array)
   array.map do |element|
     element * element
 end
-  # Use an Enumerable to square every element in the passed in array
-  # Return a new array of the results
 end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |cap|
   cap.capitalize + "!"
 end
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
 end
 
 def long_planeteer_calls(planeteer_calls)
+  
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
 end
