41bade1fe1f10a448262826fec55cbf36989d575
patrick simmons <hadroncollides@gmail.com>
2021-01-26 18:01:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 end
 
 def long_planteer_calls(long_planteer_calls)
-  long_planteer_calls.any? { |four| four.length>4 }
+  [long_planteer_calls].any? { |four| four.length>4 }
 end
 
 def find_valid_calls(planeteer_calls)
